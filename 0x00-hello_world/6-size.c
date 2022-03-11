97eeadf399f3c3b0030c3adb985006be67593fb2
root <Kimxons>
2022-03-10 17:15:23 -0800

    resolved the prevalent errors

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,11 +14,11 @@ int main(void)
 	float z;
 	long long int p;
 
-	printf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(x));
-	printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a int %lu byte(s)\n", (unsigned long)sizeof(x));
 	printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(y));
-	printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(z));
-	printf("Size oflong long int: %lu byte(s)\n", (unsigned long)sizeof(p));
+	printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(p));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(z));
 
 	return (0);
 }
