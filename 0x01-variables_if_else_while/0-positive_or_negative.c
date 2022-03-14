7d5d554ecdd05e8520f8de1bc0f271b4d76c8188
root <Kimxons>
2022-03-14 16:54:51 -0700

    betty cding style

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
- * main - entry point
+ * main - Entry point
  *
  * Return: Always 0 (Success)
- *
  */
 int main(void)
 {
@@ -16,11 +14,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
-		printf("%d is positive\n",n);	
-	else if (n == 0)	
-		printf("%d is zero\n",n);	
-	if (n < 0)	
-		printf("%d is negative\n",n);	
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
 
 	return (0);
 }
