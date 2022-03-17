59f450f25e4aa27933623df06383bc960049b3af
kimxons <kitongameshak9@gmail.com>
2022-03-17 06:31:41 +0300

    remove errors - betty-style

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,9 +2,6 @@
 
 /**
  * main - Entry point
- *
- * Description: Program that prints the first 50 Fibonacci numbers, 
- * starting with 1 and 2, followed by a new line.
  * 
  * Return: Always 0 (Success)
  */
