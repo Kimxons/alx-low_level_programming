3458738134595d9daa64dee14e03ae821c9b4d80
kimxons <kitongameshack9@gmail.com>
2022-05-24 23:48:33 +0300

    fucking fixes

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,17 +1,16 @@
 #include "main.h"
-
 /**
- * _puts - prints string
- * @str: input string
- * Return: no return
+ * _puts - Check if a number is greater than 0
+ * @str: The number to be checked
  */
-
 void _puts(char *str)
 {
-while (*str != '\0')
-{
-_putchar(*str);
-str++;
-}
-_putchar('\n');
+	int i;
+
+	for (i = 0; *str != '\0'; i++)
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
 }
